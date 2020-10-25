bbac9e321f2ac58ed183d7ee826c5eb8c9ee86a4
dev_mob_tech <dev_mob_tech@inveskey.com>
2020-10-25 17:00:31 +0530

    a

--- src/components/Index/Products.js
+++ src/components/Index/Products.js
@@ -19,6 +19,14 @@ export const Product = () => {
                             <ItemBox name="Mutli layer PCB" img={c1} />
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6">
+                            <ItemBox name="Mutli layer PCB" img={c1} />
+                        </div>
+                        <div className="col-lg-6 col-md-6">
+                            <ItemBox name="Mutli layer PCB" img={c1} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
